6e5813dd9a13ac0b6bf4cb3e2f9f55a0e95bee60
Darva <zendarva@gmail.com>
2018-04-28 22:27:55 -0500

    Fixed localization bug causing Moments not to drop.

--- src/main/java/com/gmail/zendarva/parachronology/Parachronology.java
+++ src/main/java/com/gmail/zendarva/parachronology/Parachronology.java
@@ -61,7 +61,7 @@ public class Parachronology {
 	public static Parachronology instance;
 	public static final String MODID = "parachronology";
 	public static final String MODNAME = "Parachronology";
-	public static final String VERSION = "1.5.7";
+	public static final String VERSION = "1.5.8";
 	
 	public static final CreativeTabs TAB = new CreativeTabs(Parachronology.MODID){
 
